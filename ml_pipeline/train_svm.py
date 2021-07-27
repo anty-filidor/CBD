ea304fb4e62be9364542117fdee9ec9dfd6be7ed
Michał Czuba <michal.czuba.1995@gmail.com>
2021-07-27 12:38:50 +0200

    fixed training validation pipeline

--- ml_pipeline/train_svm.py
+++ ml_pipeline/train_svm.py
@@ -3,9 +3,19 @@ from svm.dataset import Dataset
 from svm.svm_classifier import init_model, tag_data, train
 
 svm_model_path = "model.pkl"
-mode = "train"  # "tagging"
-text_corpus = "svm/data/training_set_clean_only_text.txt"
-tag_corpus = "svm/data/training_set_clean_only_tags.txt"
+
+
+def _get_train_params():
+    return "train", "svm/data/training_set_texts.txt", "svm/data/training_set_tags.txt"
+
+
+def _get_validat_params():
+    return "tag", "svm/data/test_set_only_text.txt", "svm/data/test_set_only_tags.txt"
+
+
+# mode, text_corpus, tag_corpus = get_train_params()
+mode, text_corpus, tag_corpus = _get_validat_params()
+
 
 if __name__ == "__main__":
 
@@ -17,19 +27,24 @@ if __name__ == "__main__":
             remove_stopwords=True,
             is_train=True,
         )
+        dataset.print_stats()
         acc, f1 = train(dataset, init_model(), svm_model_path)
         print(
             "Model evaluation: \n" f"Model accuracy: {acc:.01%}, " f"Model F1: {f1:.01%}"
         )
 
-    elif mode == "tagging":
+    elif mode == "tag":
         with open(svm_model_path, "rb") as file:
             model = joblib.load(file)
 
         with open(text_corpus, encoding="utf-8") as file:
             lines = [line.strip() for line in file.readlines()]
+
         dataset = Dataset(
-            texts=lines, clean_data=True, remove_stopwords=True, is_train=False
+            texts=lines,
+            clean_data=False,
+            remove_stopwords=True,
+            is_train=False,
         )
         X = dataset.df["tokens"].values
         tagged = tag_data(X, model)
